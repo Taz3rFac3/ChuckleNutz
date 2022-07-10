686c562d6b6bf54c722fe69defde9aeb50e9e452
robsinex <rob@securedoutcomes.com>
2022-07-10 16:18:00 -0400

    this is my first commmit

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pygame
+from pygame.locals import *
+
+pygame.init()
+
+
+screen  = pygame.display.set_mode((1600, 800))
+
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
 
